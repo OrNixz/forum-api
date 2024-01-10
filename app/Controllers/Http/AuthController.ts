dfd6804d05504fdeb39121efd1cb51614bb58e05
OrNixz <ramadhanafif172@gmail.com>
2024-01-10 15:14:48 +0700

    Setup login api endpoint

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -5,12 +5,33 @@ import RegisterValidator from 'App/Validators/RegisterValidator'
 export default class AuthController {
     public async register({ auth, request, response }: HttpContextContract) {
         const validateData = await request.validate(RegisterValidator)
-        const user = await User.create(validateData)
-        const token = await auth.login(user)
+        try {
+            const user = await User.create(validateData)
+            const token = await auth.login(user)
+    
+            return response.status(201).json({
+                user,
+                token,
+            })
+        } catch (error) {
+            return response.status(400).json({
+                message: error.message,
+            })
+        }
+    }
 
-        return response.status(201).json({
-            user,
-            token,
-        })
+    public async login({ auth, request, response }: HttpContextContract) {
+        const { email, password } = request.all()
+        try {
+            const token = await auth.attempt(email, password)
+            return response.status(200).json({
+                user: token.user,
+                token: token.token,
+            })
+        } catch (error) {
+            return response.status(401).json({
+                message: error.messsage,
+            })
+        }
     }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -25,5 +25,6 @@ Route.get('/', async () => {
 })
 
 Route.group(() => {
-  Route.post('/auth/register', 'AuthController.register')
+  Route.post('/auth/register', 'AuthController.register'),
+  Route.post('/auth/login', 'AuthController.login')
 }).prefix('/api')
