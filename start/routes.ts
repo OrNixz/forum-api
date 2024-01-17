df20befe35bcb19f51c7d1f1e36fe311169c6a59
OrNixz <ramadhanafif172@gmail.com>
2024-01-17 10:38:55 +0700

    Refactor route configuration

--- start/routes.ts
+++ start/routes.ts
@@ -28,11 +28,21 @@ Route.group(() => {
   Route.post('/auth/register', 'AuthController.register')
   Route.post('/auth/login', 'AuthController.login')
 
-  Route.get('/threads', 'ThreadsController.index')
-  Route.post('/threads', 'ThreadsController.store').middleware('auth')
-  Route.get('/threads/:id', 'ThreadsController.show')
-  Route.put('/threads/:id', 'ThreadsController.update').middleware('auth')
-  Route.delete('/threads/:id', 'ThreadsController.destroy').middleware('auth')
+  // Route.get('/threads', 'ThreadsController.index')
+  // Route.post('/threads', 'ThreadsController.store').middleware('auth')
+  // Route.get('/threads/:id', 'ThreadsController.show')
+  // Route.put('/threads/:id', 'ThreadsController.update').middleware('auth')
+  // Route.delete('/threads/:id', 'ThreadsController.destroy').middleware('auth')
 
-  Route.post('/threads/:thread_id/replies', 'RepliesController.store').middleware('auth')
+  Route.resource('threads', 'ThreadsController').apiOnly().middleware({
+    store: 'auth',
+    update: 'auth',
+    destroy: 'auth'
+  })
+
+  // Route.post('/threads/:thread_id/replies', 'RepliesController.store').middleware('auth')
+
+  Route.resource('threads.replies', 'RepliesController').only(['store']).middleware({
+    store: 'auth',
+  })
 }).prefix('/api')
